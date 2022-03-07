f88cacbd941499188ab92cf16cd9dee74343ea61
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-03-07 20:00:19 +0300

    Init: products

---
+++ components/sidebar.php
@@ -0,0 +1,47 @@
+<div class="sidenav-container" id="side-nav">
+  <nav class="slider_nav">
+    <div class="nav__brand">
+      <div class="nav__icon nav__icon--menu" id="nav-toggle">
+        <ion-icon name="menu-outline"></ion-icon>
+      </div>
+
+      <span href="#" class="nav__brand-logo">
+        web.script
+      </span>
+    </div>
+
+    <hr>
+
+    <ul class="nav__list">
+
+       <li class="nav__item">
+        <a href="dashboard.php" class="nav__link active">
+          <ion-icon name="home-outline" class="nav__icon"></ion-icon>
+          <span class="nav__name">Dashboard</span>
+        </a>
+      </li>
+
+      <li class="nav__item">
+        <a href="products.php" class="nav__link active">
+          <ion-icon name="folder-outline" class="nav__icon"></ion-icon>
+          <span class="nav__name">Products</span>
+        </a>
+      </li>
+
+    </ul>
+
+    <hr>
+
+    <div class="nav__user">
+      <div class="nav__user-image">
+        <img src="https://wallpapercave.com/uwp/uwp358219.jpeg" alt="profile">
+      </div>
+
+
+      <div class="nav__user-info">
+        <div class="nav__user-info-name">Fatima Alamshhor</div>
+        <div class="nav__user-info-email">fatima.almashhor@gmail.com</div>
+      </div>
+    </div>
+  </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="./public/css/sidebar.css">
+    <link rel="stylesheet" href="./public/css/main.css">
+</head>
+<body>
+<!--______________ LEFT MAIN ______________ -->
+<?php include './components/sidebar.php';?>
+<main class='main container'>
+<?php
+            include './components/dashboard.php';
+?>
+</main>
+
+    <!-- ===== IONICONS ===== -->
+    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+    <!-- ===== MAIN JS ===== -->
+    <script src="./public/js/main.js"></script>
+</body>
+</html>
