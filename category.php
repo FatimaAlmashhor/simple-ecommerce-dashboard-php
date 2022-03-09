45defb54c591fb1baf193d4270a179c3a6d030ff
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-03-09 13:36:57 +0300

    Style : update

--- category.php
+++ category.php
@@ -1,92 +1,140 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <link rel="stylesheet" href="./public/css/sidebar.css">
     <link rel="stylesheet" href="./public/css/main.css">
+    <title>Document</title>
 </head>
+
 <body>
-<!--______________ LEFT MAIN ______________ -->
-<?php include './templates/sidebar.php';?>
-<main class='main container'>
-<?php
-            // include './templates/category.php';
-            include './controllers/init.php';
-            $do = isset($_GET['do']) ? $_GET['do'] : 'add' ;
-            if($do == 'add'){
-                ?>
-                    <form action="category.php?do=add" method='post'>
-                        <input type='title' name='category' placholder='Enter Category' require/>
-                        <button type='submit'>Submit </button>
-                    </form>
-                <?php 
-                if(isset($_POST['category'])){
-                    $sql = $con->prepare("INSERT INTO  categories (category_title , is_active ) VALUES (:title , 1)");
+    <?php 
+         include './controllers/init.php';
+         include './templates/sidebar.php';
+    ?>
+    
+    <div class="page container">
+        <a class="add_section"  href="category.php?do=add">
+            <ion-icon class='add-icon' name="add-circle-outline"></ion-icon>
+        </a>
+        <div class="page_warrper">
 
-                    $sql->execute([':title' => $_POST['category']]);
-                    $con->lastInsertId();
-                }
+        <?php 
+         $do = isset($_GET['do']) ? $_GET['do'] : 'manage' ;
+         if($do == 'add'){
+             ?>
+                <div class="controll_section">
+                    <h2>Add new category</h2>
+                    <form class="form" action='category.php?do=add' method='post'  >
+                        <label for="title">Product Title</label>
+                        <input class='input'  type="title" name="title" />
+                       
+                        <button class='btn' type="submit">Add</button>
+                    </form>
+                </div>
+             <?php 
+            if(isset($_POST['title'])){
+
+
+                $sql = $con->prepare("INSERT INTO  categories 
+                    (category_title , is_active )
+                     VALUES
+                      (:title  , 1)");
+
+                $sql->execute([
+                    ':title' => $_POST['title']
+                ]);
+                $con->lastInsertId();
             }
+        }
+         else if($do == 'delete'){
+             $sql = $con->prepare("DELETE FROM  categories  WHERE category_id=:categoryid");
+             $sql->bindParam(":categoryid" , $_GET['categoryid']);
+             $sql->execute();
 
-            else if($do == 'delete'){
-                $sql = $con->prepare("DELETE FROM  categories  WHERE category_id=:catid");
-                $sql->bindParam(":catid" , $_GET['catid']);
-                $sql->execute();
-
-                
-            }else if($do == 'edit'){
-                ?>
-                    <form action="category.php?do=edit&catid=<?php echo $_GET['catid']?>" method='post'>
-                        <input type='title' name='category' placholder='Enter new Category' value='<?php echo $_GET['catid']?>' require/>
-                        <button type='submit'>Submit </button>
+             
+         }else if($do == 'edit'){
+            
+             ?>
+             <div class="controll_section">
+                    <h2>Edit this category</h2>
+                    <form class="form" action="category.php?do=edit&categoryid=
+                    <?php echo $_GET['categoryid']?>
+                    " method='post' enctype="multipart/form-data">
+                        <label for="title">Category Title</label>
+                        <input class='input' type="title" name="title" />
+                        
+                        <button class='btn' type="submit">Edit</button>
                     </form>
-                <?php 
-                 if(isset($_POST['category'])){
-                   
-                    $sql = $con->prepare("UPDATE  categories SET  category_title = :title WHERE category_id = :catid");
+                </div>
+             
+             <?php 
+             if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+                $categoryid = (isset($_GET['categoryid']) && is_numeric($_GET['categoryid']) ) ?  intval($_GET['categoryid']): 0;
+                $sql = $con->prepare("UPDATE  categories SET  category_title = :title 
+                    
+                    WHERE category_id = :categoryid");
                     
                     $sql->execute([ 	
-                        ":title" =>  $_POST['category'],
-                        ":catid" => $_GET['catid'] 
+                        ":title" =>  $_POST['title'],
+                        
+                        ":categoryid" => $categoryid
                     ]);
-                }
-            }
+             }
+         }
+       ?>
+            <div class="flex manage_section">
+                <div class="header_title">
+                    <h1>Categories</h1>
+                </div>
+                <div class='items_list'>
+                    <?php 
+                     $sql = $con->prepare("SELECT * 
+                        FROM categories
+                    ");
 
-            $sql = $con->prepare("SELECT * 
-                    FROM categories
-                   ");
+                    $sql->execute();
+                    $rows = $sql->fetchAll();
+                    foreach($rows as $row)
+                    {
+                        ?>
+                            <div class="card">
+                                <div class=" ">
+                                        <div class="ycenter-xbetween">
+                                            <h2 style="word-break: break-all;s">
+                                                <?php 
+                                                    echo $row['category_title'];
+                                                ?>
+                                            </h2>
+                                            <div class="actions">
+                                                <a href="category.php?do=edit&categoryid= <?php echo $row['category_id']; ?>">
+                                                    <ion-icon class="edit-icon" name="create-outline"></ion-icon>
+                                                </a>
+                                                <a class="delete-icon" href='category.php?do=delete&categoryid= <?php echo $row['category_id']; ?>'>
+                                                    <ion-icon name="close-circle-outline"></ion-icon>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                             </div>
+                        <?php
+                    }
+                    ?>
 
-            $sql->execute();
-            $rows = $sql->fetchAll();
-           
-            echo '
+                <div>
+            </div>
             
-            <div name=categories>';
-            foreach($rows as $row)
-            {
-                echo "
-                    <div value='$row[category_title] '> $row[category_title] ";
-                echo "<a href='category.php?do=delete&catid=". $row['category_id'] ."'>Delete</a> <span>   </span>";
-                echo "<a href='category.php?do=edit&catid=". $row['category_id'] ."'>Edit</a>";
-                echo "</div>";
-            }
-            echo "</div>
-             <a href='category.php?do=add'>Add new</a>
-            ";
+        </div>
+        
+    </div>
 
-
-
-          
-?>
-           
-</main>
-
-    <!-- ===== IONICONS ===== -->
-    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
-    <!-- ===== MAIN JS ===== -->
-    <script src="./public/js/main.js"></script>
+<script src="./public/js/main.js"></script>
 </body>
+
 </html>
--- test.php
+++ test.php
@@ -1,172 +1,94 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="./public/css/sidebar.css">
     <link rel="stylesheet" href="./public/css/main.css">
-    <title>Document</title>
 </head>
-
 <body>
-    <?php 
-         include './controllers/init.php';
-         include './templates/sidebar.php';
-    ?>
-    <div class="page container">
-        <a class="add_section"  href="products.php?do=add">
-            <ion-icon class='add-icon' name="add-circle-outline"></ion-icon>
-        </a>
-        <div class="page_warrper">
-            <div class="flex manage_section">
-                <div class="header_title">
-                    <h1>Products</h1>
-                </div>
-                <div class='items_list'>
-                    <?php 
-                     $sql = $con->prepare("SELECT * 
-                        FROM products
-                    ");
+<!--______________ LEFT MAIN ______________ -->
+<?php include './templates/sidebar.php';?>
+<main class=' main '>
 
-                    $sql->execute();
-                    $rows = $sql->fetchAll();
-                    foreach($rows as $row)
-                    {
-                        ?>
-                            <div class="card">
-                                <div class="card_warrper ">
-                                    <div class="cart_image">
-                                        <img
-                                            src="https://images.pexels.com/photos/3018845/pexels-photo-3018845.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-                                    </div>
-                                    <div>
-                                        <div class="ycenter-xbetween">
-                                            <h2>
-                                                <?php 
-                                                    echo $row['product_title'];
-                                                ?>
-                                            </h2>
-                                            <div class="actions">
-                                                <a href="products.php?do=edit&productid= <?php echo $row['product_id']; ?>">
-                                                    <ion-icon class="edit-icon" name="create-outline"></ion-icon>
-                                                </a>
-                                                <a class="delete-icon" href='products.php?do=delete&productid= <?php echo $row['product_id']; ?>'>
-                                                    <ion-icon name="close-circle-outline"></ion-icon>
-                                                </a>
-                                            </div>
-                                        </div>
-                                        <p> 
-                                            <?php 
-                                                echo $row['product_des'];
-                                            ?>
-                                        </p>
-                                        <p>$
-                                        <?php 
-                                            echo $row['product_price'];
-                                        ?>
-                                        </p>
-                                        <p>Qty : 
-                                            <?php 
-                                                echo $row['product_qty'];
-                                            ?>
-                                        </p>
-                                    </div>
-                                </div>
-                             </div>
-                        <?php
-                    }
-                    ?>
-
-                <div>
-            </div>
+<?php
+            // include './templates/category.php';
+            include './controllers/init.php';
+            $do = isset($_GET['do']) ? $_GET['do'] : 'add' ;
             
-        </div>
-        
-    </div>
-    <?php 
-         $do = isset($_GET['do']) ? $_GET['do'] : 'add' ;
-         if($do == 'add'){
-             ?>
-                <div class="controll_section">
-                    <h2>Add new product</h2>
-                    <form class="form" action='post' method='products.php?do=add' >
-                        <label for="title">Product Title</label>
-                        <input class='input' type="title" name="title" />
-                        <label for="title">Product Des</label>
-                        <input class='input' type="title" name="title" />
-                        <div class="flex ">
-                            <div>
-                                <label for="title">Product price</label>
-                                <input class='input' type="number" name="title" />
-                            </div>
-                            <div>
-                                <label for="title">Product qty</label>
-                                <input class='input' type="number" name="title" />
-                            </div>
-                        </div>
-                        <label for="title">Product image</label>
-                        <input class='input' type="file" name="title" />
-                        <button class='btn' type="submit">Add</button>
+            if($do == 'add'){
+                ?>
+                    <form class='form' action="category.php?do=add" method='post'>
+                        <input class='input ' type='title' name='category' placholder='Enter Category' require/>
+                        <button class='btn' type='submit'>Submit </button>
                     </form>
-                </div>
-             <?php 
-             if(isset($_POST['products'])){
-                 $sql = $con->prepare("INSERT INTO  products (product_title , is_active ) VALUES (:title , 1)");
+                <?php 
+                if(isset($_POST['category'])){
+                    $sql = $con->prepare("INSERT INTO  categories (category_title , is_active ) VALUES (:title , 1)");
 
-                 $sql->execute([':title' => $_POST['products']]);
-                 $con->lastInsertId();
-             }
-         }
+                    $sql->execute([':title' => $_POST['category']]);
+                    $con->lastInsertId();
+                }
+            }
+           
+            else if($do == 'delete'){
+                $sql = $con->prepare("DELETE FROM  categories  WHERE category_id=:catid");
+                $sql->bindParam(":catid" , $_GET['catid']);
+                $sql->execute();
 
-         else if($do == 'delete'){
-             $sql = $con->prepare("DELETE FROM  products  WHERE product_id=:productid");
-             $sql->bindParam(":productid" , $_GET['productid']);
-             $sql->execute();
-
-             
-         }else if($do == 'edit'){
-             ?>
-             <div class="controll_section">
-                    <h2>Edit this product</h2>
-                    <form class="form" action="products.php?do=add" method='post' >
-                        <label for="title">Product Title</label>
-                        <input class='input' type="title" name="title" />
-                        <label for="title">Product Des</label>
-                        <input class='input' type="title" name="title" />
-                        <div class="flex ">
-                            <div>
-                                <label for="title">Product price</label>
-                                <input class='input' type="number" name="title" />
-                            </div>
-                            <div>
-                                <label for="title">Product qty</label>
-                                <input class='input' type="number" name="title" />
-                            </div>
-                        </div>
-                        <label for="title">Product image</label>
-                        <input class='input' type="file" name="title" />
-                        <button class='btn' type="submit">Add</button>
+                
+            }else if($do == 'edit'){
+                ?>
+                    <form class='form' action="category.php?do=edit&catid=<?php echo $_GET['catid']?>" method='post'>
+                        <input class='input'  type='title' name='category' placholder='Enter new Category' value='<?php echo $_GET['catid']?>' require/>
+                        <button class='btn' type='submit'>Submit </button>
                     </form>
-                </div>
-             
-             <?php 
-             if(isset($_POST['products'])){
-             
-                 $sql = $con->prepare("UPDATE  products SET  product_title = :title WHERE product_id = :productid");
-                 
-                 $sql->execute([ 	
-                     ":title" =>  $_POST['products'],
-                     ":productid" => $_GET['productid'] 
-                 ]);
-             }
-         }
+                <?php 
+                 if(isset($_POST['category'])){
+                   
+                    $sql = $con->prepare("UPDATE  categories SET  category_title = :title WHERE category_id = :catid");
+                    
+                    $sql->execute([ 	
+                        ":title" =>  $_POST['category'],
+                        ":catid" => $_GET['catid'] 
+                    ]);
+                }
+            }
+
+            $sql = $con->prepare("SELECT * 
+                    FROM categories
+                   ");
+
+            $sql->execute();
+            $rows = $sql->fetchAll();
+           
+            echo '
+            
+            <div name=categories>';
+            foreach($rows as $row)
+            {
+                echo "
+                    <div value='$row[category_title] '> $row[category_title] ";
+                echo "<a class='' href='category.php?do=delete&catid=". $row['category_id'] ."'>Delete</a> <span>   </span>";
+                echo "<a class='' href='category.php?do=edit&catid=". $row['category_id'] ."'>Edit</a>";
+                echo "</div>";
+            }
+            echo "</div>
+             <a class='' href='category.php?do=add'>Add new</a>
+            ";
 
 
-        ?>
-<script src="./public/js/main.js"></script>
+
+          
+?>
+           
+</main>
+
+    <!-- ===== IONICONS ===== -->
+    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+    <!-- ===== MAIN JS ===== -->
+    <script src="./public/js/main.js"></script>
 </body>
-
 </html>
